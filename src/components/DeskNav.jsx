4e438c98e3c75f0a6272f7c1e07a577b56778dae
Morten Six <mort108v@stud.kea.dk>
2022-02-16 12:57:40 +0100

    Menu items changed to actual

--- src/components/DeskNav.jsx
+++ src/components/DeskNav.jsx
@@ -9,28 +9,28 @@ const DeskNav = ({ burgerOpen }) => {
     <nav className="desk-nav open">
       <ul className="desk-menu-nav open">
         <li className={className}>
-          <a href="#!" className="desk-menu-nav__link signin-btn ">
-            {"Sign In!"}
-          </a>
-        </li>
-        <li className={className}>
-          <a href="#!" className="desk-menu-nav__link signup-btn">
-            {"Sign Up!"}
-          </a>
-        </li>
-        <li className={className}>
-          <a href="#!" className="desk-menu-nav__link guest-btn">
-            {"Guest <3"}
-          </a>
-        </li>
-        <li className={className}>
           <a href="#!" className="desk-menu-nav__link about-btn">
             {"About"}
           </a>
         </li>
         <li className={className}>
-          <a href="#!" className="desk-menu-nav__link some-btn">
-            {"Some"}
+          <a href="#!" className="desk-menu-nav__link background-btn">
+            {"Background"}
+          </a>
+        </li>
+        <li className={className}>
+          <a href="#!" className="desk-menu-nav__link work-btn">
+            {"Work"}
+          </a>
+        </li>
+        <li className={className}>
+          <a href="#!" className="desk-menu-nav__link goals-btn">
+            {"Goals"}
+          </a>
+        </li>
+        <li className={className}>
+          <a href="#!" className="desk-menu-nav__link blockchain-btn">
+            {"Blockchain"}
           </a>
         </li>
       </ul>
